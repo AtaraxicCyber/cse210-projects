9130ad6a5fb4ae228adb129edf0018ada0c4c5d2
Teagan Lewis <lewiteag@byui.edu>
2026-01-10 22:11:57 -0700

    prep2 assginment complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade?");
+        string percentGradeString = Console.ReadLine();
+        int percentGrade = int.Parse(percentGradeString);
+        string grade;
+
+        if (percentGrade >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentGrade >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentGrade >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentGrade >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"You earned a {grade}");
+        if (percentGrade >= 70)
+        {
+            Console.WriteLine("You passed. Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("You failed. Better luck next time!");
+        }
+
     }
 }
